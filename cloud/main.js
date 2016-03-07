b4d2d91e812854bb120d9587ab130ca3b908bb76
Srikant Kammila <srikanth1234@gmail.com>
2016-03-07 00:19:46 -0800

    db query test

--- cloud/main.js
+++ cloud/main.js
@@ -24,7 +24,7 @@ Parse.Cloud.define('findUsers', function(req, res) {
 	  } else {
 	    console.log("connected to database");
 	  }
-	  var collection = db.collection('Driver');
+	  var collection = db.collection('_User');
 	  collection.find({}).toArray(function(err, docs) {
 	        if (err) {
 	          return console.error(err)
